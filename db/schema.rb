494128698d92fda2d40137d5c2e11f1cbfb0cb09
Ryan <nygardryan@yahoo.com>
2017-04-14 14:00:58 -0600

    added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170413223613) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
+    t.string   "subtitle"
     t.text     "body"
     t.text     "main_image"
     t.text     "thumb_image"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog| 
+    Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
+    ut fermentum massa justo sit amet risus. Cum sociis natoque 
+    penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+    Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac 
+    consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis euismod semper."
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+Skill.create!(
+  title: "Rails #{skill}",
+  percent_utilized: 20
+)
+end
+puts "5 skill points created"
+
+9.times do |port_item|
+  Portfolio.create!(
+      title: "Portfolio title: #{port_item}",
+      subtitle: "My great feet!",
+      body: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, 
+      eget lacinia odio sem nec elit. Nulla vitae elit libero,
+       a pharetra augue. Vestibulum id ligula porta felis euismod semper.",
+      main_image: "http://placehold.it/600x400",
+      thumb_image: "http://placehold.it/350x200"
+    )
+end
+  puts "created 9 portfolios"
