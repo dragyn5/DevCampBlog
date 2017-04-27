42aa134598f0844311694835f605e53b1b02d51e
Ryan <nygardryan@yahoo.com>
2017-04-27 17:04:36 -0600

    implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,43 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },      
+      {
+        url: about_path,
+        title: 'About me'
+      },      
+      {
+        url: contact_path,
+        title: 'Contact'
+      },      
+      {
+        url: blogs_path,
+        title: 'Blogs'
+      },      
+      {
+        url: portfolios_path,
+        title: 'Portfolios'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links <<   "<#{tag_type}><a href='#{root_path}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>" 
+    end
+
+  nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path    
+  end
 end
