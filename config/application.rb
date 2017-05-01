5654f5395c8e4f531122a9432e9b50d4a39c76b7
Ryan <nygardryan@yahoo.com>
2017-05-01 10:19:17 -0600

    Finished carrierwave installation

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require "dotenv-rails"
+
+
 
 module DevcampPortfolio
   class Application < Rails::Application
