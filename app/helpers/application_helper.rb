3e0824973ac9bf113c9bad66e7ffda1b5c828f49
Ryan <nygardryan@yahoo.com>
2017-05-01 16:29:16 -0600

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,3 +56,17 @@ module ApplicationHelper
     "active" if current_page? path    
   end
 end
+
+
+def alerts
+  alert = (flash[:alert] || flash[:error] || flash[:notice])
+  if alert
+    alert_generator alert
+  end
+end
+
+def alert_generator msg
+   js add_gritter(msg, title: "Ryan Nygards Portfolio", sticky: false)
+end
+
+
