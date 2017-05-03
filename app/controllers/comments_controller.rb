21c0bb055e0ed64fdaa23e136b8ee655775e3068
Ryan <nygardryan@yahoo.com>
2017-05-03 14:29:46 -0600

    added comment section to blogs page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+end
+
+def comment_params
+  params.require(:comment).permit(:content)
 end
