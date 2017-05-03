c40e3a9bee73258e02be03bebe94f9663ffc9c4c
Ryan <nygardryan@yahoo.com>
2017-05-03 13:51:49 -0600

    Implemented redis library and created comment generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501173913) do
+ActiveRecord::Schema.define(version: 20170503194424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20170501173913) do
     t.integer  "status",     default: 0
     t.string   "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -90,5 +100,7 @@ ActiveRecord::Schema.define(version: 20170501173913) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "blogs"
+  add_foreign_key "comments", "users"
   add_foreign_key "technologies", "portfolios"
 end
